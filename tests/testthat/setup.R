aeef1f4a0c8481cd679dc72604655ad1a0901fdd
hansvancalster <hans.vancalster@inbo.be>
2026-04-24 14:49:42 +0200

    fix linters

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -4,7 +4,7 @@
 create_test_wkb <- function() {
   # A simple 1x1 degree square near the equator
   poly <- sf::st_polygon(
-    list(matrix(c(0,0, 0,1, 1,1, 1,0, 0,0), ncol=2, byrow=TRUE))
+    list(matrix(c(0, 0, 0, 1, 1, 1, 1, 0, 0, 0), ncol = 2, byrow = TRUE))
   )
   poly_sfc <- sf::st_sfc(poly, crs = 4326)
   sf::st_as_binary(poly_sfc, EWKB = FALSE)[[1]]
